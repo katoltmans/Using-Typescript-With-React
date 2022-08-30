de008b64d07392a61cc8f3a5bc215b2b9b048acb
katoltmans <katoltmans@msn.com>
2022-08-30 13:25:59 -0400

    Add intersection type examples

--- simple-typescript/src/intersectionTypeExample.ts
+++ simple-typescript/src/intersectionTypeExample.ts
@@ -1,18 +1,37 @@
-"use strict";
-function X(obj) {
+interface IA {
+    a: number;
+}
+
+interface IB {
+    b: number;
+}
+
+interface IC {
+    c: number;
+}
+
+function X(obj: IA & IB & IC) {
     return obj.a + obj.b + obj.c;
 }
+
 // Intersection type = combines multiple objects into one
-function combine(objA, objB) {
-    return Object.assign(Object.assign({}, objA), objB);
+function combine<ObjA extends object, ObjB extends object>(
+    objA: ObjA,
+    objB: ObjB
+): ObjA & ObjB {
+    return { ...objA, ...objB };
 }
+
 const objA = { a: 1 };
 const objB = { b: 2 };
 const resultObj = combine(objA, objB);
+
 // Will return any - need to avoid a return of any as much as we can
+
 // function combine(objA, objB) {
 //     return { ...objA, ...objB };
 // }
+
 // const objA = { a: 1 };
 // const objB = { b: 2 };
 // const resultObj = combine(objA, objB);
