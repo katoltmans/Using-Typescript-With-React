501a06acfec74ab8808a4ee821d6fb5dc3d51607
katoltmans <katoltmans@msn.com>
2022-08-30 14:06:26 -0400

    Add .d.ts file to declare module 'react' and use typed function example

---
+++ simple-typescript/src/externalPackagesExamples.ts
@@ -0,0 +1,13 @@
+// When installing packages - install types from the DefinitelyTypes repo is the package does not come with native types
+{
+    /* To insure the types version of a package is downloaded, run:
+
+npm i -D @types/npm-package-name
+
+*/
+}
+
+import ApolloCLient from "apollo-client"; // (Installed with: npm i -S apollo-CLient)
+import * as React from "react"; // (Installed with: npm i -D @types.react)
+
+React.someFn();
---
+++ simple-typescript/src/react.d.ts
@@ -0,0 +1,5 @@
+// Example of adding a new declaration (.d.ts) file containing `declare module'react';`
+
+declare module "react" {
+    export function someFn(): number;
+}
